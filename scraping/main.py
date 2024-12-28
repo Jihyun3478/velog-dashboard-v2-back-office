ffcf9055dd7c5ad91972b6ae0a9e841569fc7776
Nuung <qlgks1@naver.com>
2024-12-28 23:36:46 +0900

    feature: scraping 클래스화, 그에 따라 그룹 3분할 동시성 코드로 변경

--- scraping/main.py
+++ scraping/main.py
@@ -18,192 +18,183 @@ from scraping.apis import (
 )
 from users.models import User
 
-logger = logging.getLogger("scraping")
 
-env = environ.Env()
+class Scraper:
+    def __init__(self, group_range: range):
+        self.logger = logging.getLogger("scraping")
+        self.env = environ.Env()
+        self.group_range = group_range
 
-
-def get_local_now() -> datetime:
-    """django timezone 을 기반으로 하는 실제 local의 now datatime"""
-
-    utc_now = timezone.now()
-    local_now: datetime = timezone.localtime(
-        utc_now,
-        timezone=timezone.get_current_timezone(),
-    )
-    return local_now
-
-
-async def update_old_tokens(
-    user: User,
-    aes_encryption: AESEncryption,
-    user_cookies: dict[str, str],
-    old_access_token: str,
-    old_refresh_token: str,
-) -> None:
-    """토큰 만료로 인한 토큰 업데이트"""
-    response_access_token, response_refresh_token = (
-        user_cookies["access_token"],
-        user_cookies["refresh_token"],
-    )
-    if response_access_token != old_access_token:
-        new_access_token = aes_encryption.encrypt(response_access_token)
-        user.access_token = new_access_token
-    if response_refresh_token != old_refresh_token:
-        new_refresh_token = aes_encryption.encrypt(response_refresh_token)
-        user.refresh_token = new_refresh_token
-
-    try:
-        await user.asave(update_fields=["access_token", "refresh_token"])
-        logger.info(
-            f"Succeeded to update tokens. (user velog uuid: {user.velog_uuid})"
+    @staticmethod
+    def get_local_now() -> datetime:
+        """django timezone 을 기반으로 하는 실제 local의 now datetime"""
+        utc_now = timezone.now()
+        local_now: datetime = timezone.localtime(
+            utc_now, timezone=timezone.get_current_timezone()
         )
-    except Exception as e:
-        logger.error(
-            f"Failed to update tokens. {e} (user velog uuid: {user.velog_uuid})"
+        return local_now
+
+    async def update_old_tokens(
+        self,
+        user: User,
+        aes_encryption: AESEncryption,
+        user_cookies: dict[str, str],
+        old_access_token: str,
+        old_refresh_token: str,
+    ) -> None:
+        """토큰 만료로 인한 토큰 업데이트"""
+        response_access_token, response_refresh_token = (
+            user_cookies["access_token"],
+            user_cookies["refresh_token"],
+        )
+        if response_access_token != old_access_token:
+            user.access_token = aes_encryption.encrypt(response_access_token)
+        if response_refresh_token != old_refresh_token:
+            user.refresh_token = aes_encryption.encrypt(response_refresh_token)
+
+        try:
+            await user.asave(update_fields=["access_token", "refresh_token"])
+            self.logger.info(
+                f"Succeeded to update tokens. (user velog uuid: {user.velog_uuid})"
+            )
+        except Exception as e:
+            self.logger.error(
+                f"Failed to update tokens. {e} (user velog uuid: {user.velog_uuid})"
+            )
+
+    async def bulk_create_posts(
+        self, user: User, fetched_posts: list[dict[str, str]]
+    ) -> bool:
+        """Post 객체를 대량으로 생성"""
+        try:
+            await Post.objects.abulk_create(
+                [
+                    Post(
+                        post_uuid=post["id"],
+                        title=post["title"],
+                        user=user,
+                        released_at=post["released_at"],
+                    )
+                    for post in fetched_posts
+                ],
+                ignore_conflicts=True,
+                batch_size=500,
+            )
+            return True
+        except Exception as e:
+            self.logger.error(
+                f"Failed to bulk create posts. {e} (user velog uuid: {user.velog_uuid})"
+            )
+            return False
+
+    async def update_daily_statistics(
+        self, post: dict[str, str], stats: dict[str, str]
+    ) -> None:
+        """PostDailyStatistics를 업데이트 또는 생성 (upsert)"""
+        post_obj = await sync_to_async(Post.objects.get)(post_uuid=post["id"])
+        today = self.get_local_now().date()
+        (
+            daily_stats,
+            created,
+        ) = await PostDailyStatistics.objects.aget_or_create(
+            post=post_obj,
+            date=today,
+            defaults={
+                "daily_view_count": stats["data"]["getStats"]["total"],  # type: ignore
+                "daily_like_count": stats.get("like_count", 0),
+            },
+        )
+        if not created:
+            daily_stats.daily_view_count = stats["data"]["getStats"]["total"]  # type: ignore
+            daily_stats.daily_like_count = stats.get("like_count", 0)
+            await daily_stats.asave(
+                update_fields=["daily_view_count", "daily_like_count"]
+            )
+
+    async def process_user(
+        self, user: User, session: aiohttp.ClientSession
+    ) -> None:
+        """유저 데이터를 처리"""
+        aes_key_index = (user.group_id % 100) % 10
+        aes_key = self.env(f"AES_KEY_{aes_key_index}").encode()
+        aes_encryption = AESEncryption(aes_key)
+        old_access_token = aes_encryption.decrypt(user.access_token)
+        old_refresh_token = aes_encryption.decrypt(user.refresh_token)
+
+        # 토큰 유효성 검증
+        user_cookies, user_data = await fetch_velog_user_chk(
+            session, old_access_token, old_refresh_token
+        )
+        if not (user_data or user_cookies):
+            return
+
+        if user_data["data"]["currentUser"] is None:  # type: ignore
+            self.logger.warning(
+                f"Failed to fetch user data because of wrong tokens. (user velog uuid: {user.velog_uuid})"
+            )
+            return
+
+        if user_cookies:
+            await self.update_old_tokens(
+                user,
+                aes_encryption,
+                user_cookies,
+                old_access_token,
+                old_refresh_token,
+            )
+
+        username = user_data["data"]["currentUser"]["username"]  # type: ignore
+        fetched_posts = await fetch_all_velog_posts(
+            session, username, old_access_token, old_refresh_token
         )
 
+        await self.bulk_create_posts(user, fetched_posts)
 
-async def bulk_create_posts(
-    user: User, fetched_posts: list[dict[str, str]]
-) -> bool:
-    """post 를 bulk로 만드는 함수"""
-    try:
-        await Post.objects.abulk_create(
-            [
-                Post(
-                    post_uuid=post["id"],
-                    title=post["title"],
-                    user=user,
-                    released_at=post["released_at"],
-                )
-                for post in fetched_posts
-            ],
-            ignore_conflicts=True,
-            batch_size=500,
-        )
-        return True
-    except Exception as e:
-        logger.error(
-            f"Failed to bulk create posts. {e} (user velog uuid: {user.velog_uuid})"
-        )
-        return False
+        tasks = [
+            fetch_post_stats(post["id"], old_access_token, old_refresh_token)
+            for post in fetched_posts
+        ]
+        statistics_results = await asyncio.gather(*tasks)
 
+        for post, stats in zip(fetched_posts, statistics_results):
+            if stats:
+                await self.update_daily_statistics(post, stats)
 
-async def update_daily_statistics(
-    post: dict[str, str], stats: dict[str, str]
-) -> None:
-    """PostDailyStatistics를 업데이트 또는 생성 (upsert)"""
-    post_obj = await sync_to_async(Post.objects.get)(post_uuid=post["id"])
-    today = get_local_now().date()
-    daily_stats, created = await PostDailyStatistics.objects.aget_or_create(
-        post=post_obj,
-        date=today,
-        defaults={
-            "daily_view_count": stats["data"]["getStats"]["total"],  # type: ignore
-            "daily_like_count": post.get("likes", 0),
-        },
-    )
-    if not created:
-        # 기존 통계를 업데이트
-        daily_stats.daily_view_count = stats["data"]["getStats"]["total"]  # type: ignore
-        daily_stats.daily_like_count = post.get("likes", 0)
-        await daily_stats.asave(
-            update_fields=["daily_view_count", "daily_like_count"]
+    async def run(self) -> None:
+        """스크래핑 작업 실행"""
+        self.logger.info(
+            f"Start scraping velog posts and statistics. {self.get_local_now().isoformat()}"
         )
+        users: list[User] = [
+            user
+            async for user in User.objects.filter(
+                group_id__in=self.group_range
+            )
+        ]
+        async with aiohttp.ClientSession() as session:
+            for user in users:
+                await self.process_user(user, session)
 
 
 async def main() -> None:
-    logger.info(
-        f"Start scraping velog posts and statistics. {get_local_now().isoformat()}"
-    )
+    """3개의 동시성 작업으로 유저 그룹 처리"""
+    group_ranges = [
+        range(1, 334),
+        range(334, 667),
+        range(667, 1001),
+    ]
 
-    # TODO: group별 batch job 실행 방식 확정 후 리팩토링
-    users: list[User] = [user async for user in User.objects.all()]
-    async with aiohttp.ClientSession() as session:
-        for user in users:
-            encrypted_access_token = user.access_token
-            encrypted_refresh_token = user.refresh_token
-
-            # TODO: HARD_CODING 수정
-            aes_key_index = (user.group_id % 100) % 10
-            aes_key = env(f"AES_KEY_{aes_key_index}").encode()
-            aes_encryption = AESEncryption(aes_key)
-            old_access_token = aes_encryption.decrypt(encrypted_access_token)
-            old_refresh_token = aes_encryption.decrypt(encrypted_refresh_token)
-
-            # 토큰 유효성 검증
-            user_cookies, user_data = await fetch_velog_user_chk(
-                session,
-                old_access_token,
-                old_refresh_token,
-            )
-
-            # 유저 정보 조회 실패
-            if not (user_data or user_cookies):
-                continue
-
-            # 잘못된 토큰으로 인한 유저 정보 조회 불가
-            if user_data["data"]["currentUser"] is None:  # type: ignore
-                logger.warning(
-                    f"Failed to fetch user data because of wrong tokens. (user velog uuid: {user.velog_uuid})"
-                )
-                continue
-
-            # 토큰 만료로 인한 토큰 업데이트
-            if user_cookies:
-                await update_old_tokens(
-                    user,
-                    aes_encryption,
-                    user_cookies,
-                    old_access_token,
-                    old_refresh_token,
-                )
-
-            # username으로 velog post 조회
-            username = user_data["data"]["currentUser"]["username"]  # type: ignore
-            fetched_posts = await fetch_all_velog_posts(
-                session,
-                username,
-                old_access_token,
-                old_refresh_token,
-            )
-
-            # 새로운 post 저장
-            await bulk_create_posts(user, fetched_posts)
-
-            # 통계 API 호출 태스크 생성
-            tasks = []
-            for post in fetched_posts:
-                tasks.append(
-                    fetch_post_stats(
-                        post["id"],
-                        old_access_token,
-                        old_refresh_token,
-                    )
-                )
-
-            # 병렬 처리 (모든 통계 API 호출 수행)
-            statistics_results = await asyncio.gather(*tasks)
-
-            # PostDailyStatistics 업데이트
-            for post, stats in zip(fetched_posts, statistics_results):
-                if stats:  # 통계가 유효한 경우에만 업데이트
-                    await update_daily_statistics(post, stats)
-
-            logger.info(
-                f"Succeeded to update {len(fetched_posts)} posts stats."
-                f"(user velog uuid: {user.velog_uuid}), email: {user.email}"
-            )
+    scrapers = [Scraper(group_range) for group_range in group_ranges]
+    await asyncio.gather(*(scraper.run() for scraper in scrapers))
 
 
 # Django에서 발생하는 RuntimeWarning 무시
-# datetime 을 date 로 바꾸면서 timezone 을 적용하지 않은 것으로 착각해서 발생하는 warning, 무시
 warnings.filterwarnings(
     "ignore",
     message=r"DateTimeField .* received a naive datetime",
     category=RuntimeWarning,
 )
 
-
-asyncio.run(main())
+# 실행
+if __name__ == "__main__":
+    asyncio.run(main())
