9205f94407a1b9d9ee78eedf555cd12007e860b0
Nuung <qlgks1@naver.com>
2025-02-16 18:49:52 +0900

    modify: 배포 환경 세팅 추가

--- backoffice/settings/prod.py
+++ backoffice/settings/prod.py
@@ -1,5 +1,20 @@
 from .base import *  # noqa: F401, F403
 
-ALLOWED_HOSTS = ["*"]  # TODO: 추후 도메인 설정 후 변경
+ALLOWED_HOSTS = ["admin-vd2.kro.kr"]
 
-# TODO: 추후 도메인 설정 후 CSRF_TRUSTED_ORIGINS, CORS_ALLOWED_ORIGINS에 추가
+DEBUG = False
+
+INTERNAL_IPS = []  # 프로덕션 환경에서는 빈 리스트로 설정
+
+CSRF_TRUSTED_ORIGINS = ["https://admin-vd2.kro.kr"]
+
+CORS_ALLOWED_ORIGINS = ["https://admin-vd2.kro.kr"]
+
+# # 추가 보안 설정
+# SECURE_SSL_REDIRECT = True  # HTTP를 HTTPS로 리다이렉트
+# SESSION_COOKIE_SECURE = True  # HTTPS에서만 쿠키 전송
+# CSRF_COOKIE_SECURE = True  # HTTPS에서만 CSRF 쿠키 전송
+# SECURE_BROWSER_XSS_FILTER = True  # XSS 필터링 활성화
+# SECURE_HSTS_SECONDS = 31536000  # HSTS 활성화 (1년)
+# SECURE_HSTS_INCLUDE_SUBDOMAINS = True  # HSTS 서브도메인 포함
+# SECURE_HSTS_PRELOAD = True  # HSTS 프리로드 활성화
